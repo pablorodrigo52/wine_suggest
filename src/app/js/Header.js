ad7aba5ecf756b572f79ad35415ac245fc9ab9b2
Pablo Rodrigo <prlsantos@dcomp.ufs.br>
2020-08-10 10:35:03 -0300

    Adjusting project structure:  - Removing files unecessary;  - Updating readme;  - Adjusting style and structure project folder

---
+++ src/app/js/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../css/Header.css';
+
+function Header(){
+    return (
+        <section className='header'>
+        </section>
+    );
+}
+
+export default Header;
