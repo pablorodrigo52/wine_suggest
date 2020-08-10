69377b05e8033b0ac6d0bbc0a3ab2fb9a7c41b89
Pablo Rodrigo <prlsantos@dcomp.ufs.br>
2020-08-10 11:18:46 -0300

    header component

--- src/app/js/Header.js
+++ src/app/js/Header.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import '../css/Header.css';
 
+import avatar from '../assets/avataaars.png';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faWineGlassAlt } from '@fortawesome/free-solid-svg-icons'
+
+
 function Header(){
     return (
         <section className='header'>
+            <div className='icon childFlex'>
+                <FontAwesomeIcon icon={faWineGlassAlt} />
+            </div>
+            <div className='menu childFlex'>
+                <a href='#'>Home</a>
+                <a href='#'>About</a>
+            </div>
+            <div className='avatar childFlex'>
+                <div className='me'>
+                    <img id='avatar' src={avatar} alt="Me"/>
+                </div>
+            </div>
         </section>
     );
 }
