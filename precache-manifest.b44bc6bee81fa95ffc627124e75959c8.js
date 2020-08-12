c2188e64ca9468279a27e53d87704662840cf46e
Pablo Rodrigo <prlsantos@dcomp.ufs.br>
2020-08-12 11:31:31 -0300

    Updates

---
+++ precache-manifest.b44bc6bee81fa95ffc627124e75959c8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7621227ca8cafe9b0706754f0938ed6b",
+    "url": "/wine_suggest/index.html"
+  },
+  {
+    "revision": "7986241bd4c741ce2ddb",
+    "url": "/wine_suggest/static/css/main.3572dbfd.chunk.css"
+  },
+  {
+    "revision": "82ad956c7ecf1facf807",
+    "url": "/wine_suggest/static/js/2.1fd56dde.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wine_suggest/static/js/2.1fd56dde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7986241bd4c741ce2ddb",
+    "url": "/wine_suggest/static/js/main.b171a97f.chunk.js"
+  },
+  {
+    "revision": "9a2cfa0dc08b7a3c35ed",
+    "url": "/wine_suggest/static/js/runtime-main.0993e70d.js"
+  },
+  {
+    "revision": "4e269eeb021321ba4a15cbd17f58de2f",
+    "url": "/wine_suggest/static/media/avataaars.4e269eeb.png"
+  },
+  {
+    "revision": "51201774d084ce1c230bf3560b521daf",
+    "url": "/wine_suggest/static/media/wine_sample.51201774.png"
+  }
+]);
